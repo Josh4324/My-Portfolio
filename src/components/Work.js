2ab673ff78ebbe7427de2cc2b400ab12326b281e
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-03-12 10:49:49 +0100

    new update

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,26 +1,92 @@
-import React, { useRef } from "react";
+import React from "react";
 import NavBar from "./NavBar";
 
 const Work = () => {
-  const imgRef = useRef(null);
-
-  const fc = () => {
-    imgRef.current.src = "";
-  };
   return (
     <div>
       <div className="project">
         <h1 className="proh"> Projects </h1>
 
-        <div>
-          <div className="card">
-            <img
-              src="solar.png"
-              alt=""
-              className="card-img"
-              onMouseEnter={fc}
-              ref={imgRef}
-            />
+        <div className="container">
+          <div className="card item">
+            <img src="solar.png" alt="" className="card-img" />
+            <p>Solar Calculator</p>
+            <p>
+              A Progressive Web App calculates solar power needed to power an
+              household. Built by Me and my team mates at HNGI6
+            </p>
+            <p className="np">
+              <a
+                href="https://hngi.github.io/Poseidon-Solar-Calculator/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit Webiste
+              </a>
+            </p>
+            <p className="np">
+              <a
+                href="https://github.com/hngi/Poseidon-Solar-Calculator"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github Code
+              </a>
+            </p>
+          </div>
+
+          <div className="card item">
+            <img src="solar.png" alt="" className="card-img" />
+            <p>Solar Calculator</p>
+            <p>
+              A Progressive Web App calculates solar power needed to power an
+              household. Built by Me and my team mates at HNGI6
+            </p>
+            <p className="np">
+              <a
+                href="https://hngi.github.io/Poseidon-Solar-Calculator/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit Webiste
+              </a>
+            </p>
+            <p className="np">
+              <a
+                href="https://github.com/hngi/Poseidon-Solar-Calculator"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github Code
+              </a>
+            </p>
+          </div>
+
+          <div className="card item">
+            <img src="solar.png" alt="" className="card-img" />
+            <p>Solar Calculator</p>
+            <p>
+              A Progressive Web App calculates solar power needed to power an
+              household. Built by Me and my team mates at HNGI6
+            </p>
+            <p className="np">
+              <a
+                href="https://hngi.github.io/Poseidon-Solar-Calculator/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit Webiste
+              </a>
+            </p>
+            <p className="np">
+              <a
+                href="https://github.com/hngi/Poseidon-Solar-Calculator"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github Code
+              </a>
+            </p>
           </div>
         </div>
       </div>
